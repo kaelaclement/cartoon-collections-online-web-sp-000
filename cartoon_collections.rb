307ce1d86b2a06ffd39c41d125a89df24182d41e
Kaela Clement <stephanie.kaela@gmail.com>
2018-10-25 20:22:35 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,9 +16,9 @@ def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |cheese|
     if ingredients.include?(cheese)
-      cheese
+      return cheese
     else
-      nil
+      return nil
     end
   end
 
